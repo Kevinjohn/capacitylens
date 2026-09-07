b6ae3afed4dfc69d6e11c9da782f81d38f40b456
Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>
2026-09-07 11:57:41 +0100

    refactor: name batch projection inputs and module
    Signed-off-by: Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>

--- server/src/routes/batch/types.ts
+++ server/src/routes/batch/types.ts
@@ -2,7 +2,7 @@ import { type AppData } from "@capacitylens/shared/types/entities";
 import type { FastifyRequest } from "fastify";
 import type { LocalAccountFlows } from "../../accounts/localAccountFlows";
 import type { AuditRecord } from "../../audit";
-import { BatchStateProjection } from "../../batchProjection";
+import { BatchStateProjection } from "../../BatchStateProjection";
 import { type Db } from "../../db";
 import { type SanitizeWriteOptions } from "../../fieldPolicy";
 import { type SyncOrder } from "../../syncOrdering";
