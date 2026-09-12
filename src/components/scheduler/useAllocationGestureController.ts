57af7b5fb48aadfa2125b21206d64bf86458615e
Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>
2026-09-12 19:23:49 +0100

    refactor: narrow preview allocation input
    Signed-off-by: Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>

--- src/components/scheduler/useAllocationGestureController.ts
+++ src/components/scheduler/useAllocationGestureController.ts
@@ -2,7 +2,7 @@ import { useState, type PointerEvent as ReactPointerEvent } from "react";
 import { m } from "@/i18n";
 import { effectiveWorkingWeek } from "@capacitylens/shared/lib/effectiveWorkingWeek";
 import { rangesOverlap } from "@capacitylens/shared/lib/dateMath";
-import { MAX_HOURS_PER_DAY, type ID, type Weekday } from "@capacitylens/shared/types/entities";
+import { MAX_HOURS_PER_DAY, type Allocation, type ID, type Weekday } from "@capacitylens/shared/types/entities";
 import { useDragResize, type DragResizePreviewInput, type Pointer } from "../../hooks/useDragResize";
 import { resolveErrorMessage } from "../../lib/errorMessage";
 import { applyGesture, type DateRange, type DragMode } from "../../lib/gestureMath";
@@ -60,7 +60,7 @@ function refuseIneffectiveResize(bar: BarLayout, mode: DragMode, resourceId: ID)
 }
 
 interface ReadPreviewDatesInput {
-  bar: Pick<BarLayout, "allocation">;
+  bar: { allocation: Pick<Allocation, "resourceId" | "startDate" | "endDate" | "ignoreWeekends"> };
   runtime: Pick<GestureRuntime, "previewDaysRef">;
   input: Pick<DragResizePreviewInput, "mode" | "deltaDays">;
   destination: Pick<LaneSnapshot, "id"> | null;
