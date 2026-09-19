8b0abf3d595a285fd65e0af9a5824dc0927f1661
Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>
2026-09-19 02:45:36 +0100

    Add branded sign-in provider buttons
    Signed-off-by: Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>

--- src/auth/LoginScreen.tsx
+++ src/auth/LoginScreen.tsx
@@ -286,6 +286,7 @@ function ProviderButtons({
           provider={provider}
           label={m.login_continue_with({ provider: provider.label })}
           googleLabel={m.login_sign_in_with_google()}
+          microsoftLabel={m.login_sign_in_with_microsoft()}
           onClick={() => void signInWithProvider(provider)}
           disabled={busy}
         />
