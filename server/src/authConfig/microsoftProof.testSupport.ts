d1aa5f318d8a21885fcfafe558b05a19bf961b81
Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>
2026-09-24 14:43:46 +0100

    Format the Microsoft proof test mail mock
    Signed-off-by: Kevinjohn Gallagher <kevinjohngallagher@hotmail.com>

--- server/src/authConfig/microsoftProof.testSupport.ts
+++ server/src/authConfig/microsoftProof.testSupport.ts
@@ -9,8 +9,7 @@ vi.mock("nodemailer", () => ({
   default: {
     createTransport: () => ({
       sendMail: async (message: { to: string; text: string }) => {
-        if (mailFailure.enabled)
-          throw Object.assign(new Error(`SMTP rejected ${message.to}`), { code: "EENVELOPE" });
+        if (mailFailure.enabled) throw Object.assign(new Error(`SMTP rejected ${message.to}`), { code: "EENVELOPE" });
         sentMessages.push(message);
       },
     }),
